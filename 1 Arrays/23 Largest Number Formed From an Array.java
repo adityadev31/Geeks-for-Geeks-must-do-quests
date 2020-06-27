7e1a506d4de29ccba97e5ea79425a487d0256670
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-06-27 16:05:32 +0530

    Create 23 Largest Number Formed From an Array.java

---
+++ 1 Arrays/23 Largest Number Formed From an Array.java
@@ -0,0 +1,58 @@
+/**
+
+
+Given a list of non negative integers, arrange them in such a manner that they form the largest number possible.The result is going to be very large, hence return the result in the form of a string.
+
+Input:
+The first line of input consists number of the test cases. The description of T test cases is as follows:
+The first line of each test case contains the size of the array, and the second line has the elements of the array.
+
+Output:
+In each separate line print the largest number formed by arranging the elements of the array in the form of a string.
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ N ≤ 102
+0 ≤ A[i] ≤ 103
+
+Example:
+Input:
+2
+5
+3 30 34 5 9
+4
+54 546 548 60
+
+Output:
+9534330
+6054854654
+
+
+
+**/
+
+
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+ {
+    /**       USING ARRAYS SORT AND COMPARATOR         **/
+     
+     
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while(t-->0){
+		    br.readLine();
+            String strarr[] = br.readLine().split(" ");
+            Arrays.sort(strarr, ((x,y)->
+                (x+y).compareTo((y+x))>0?-1:1
+            ));
+            System.out.println(String.join("",strarr));
+		}
+	}
+}
