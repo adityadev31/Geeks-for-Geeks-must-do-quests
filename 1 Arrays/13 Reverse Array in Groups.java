1269e0790c4267f9d2dcfbc149b488d3bc232994
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-06-27 07:18:19 +0530

    Update 13 Reverse Array in Groups.java

--- 1 Arrays/13 Reverse Array in Groups.java
+++ 1 Arrays/13 Reverse Array in Groups.java
@@ -34,39 +34,75 @@ Testcase 1: Reversing groups in size 3, first group consists of elements 1, 2, 3
 
 import java.util.*;
 
+ /*
+   
+   S L I D I N G.  W I N D O W.  M E T H O D. 
+   
+   step1 - making a window of len k (left=0 && right=k-1)
+   step2 - swap all elements in window
+   step3 - move window to the next k (left=right+1 && right=right+k)
+   step4 - do above while(right < n)
+   step5 - last swapping for the left overs
+   
+   */
+
  class Main
  {
-   public static void reverse (int[] arr, int k){
-     Stack<Integer> st = new Stack<>();
-     int n = arr.length;
-     int i=0, j=0, p=0;
-     int[] ans = new int[n];
-     while(i<n){
-       if(j<k){             // k times pushing arr -> stack
-         st.push(arr[i++]);
-         j++;
+   
+   public static void swap(int[] arr, int a, int b){
+     int temp = arr[a];
+     arr[a] = arr[b];
+     arr[b] = temp;
+   }
+   
+   
+   public static void reverse(int[] arr, int n, int k){
+     /*
+         w1 = left side of the window
+         w2 = left side of the window
+         l = left swapper
+         r = right swapper
+         k = checker
+     */
+     int w1=0, w2=k-1;
+     int l=w1, r=w2;
+     
+     // main loop
+     while(w2 < n){
+       while(l < r){
+         swap(arr, l, r);
+         l++; r--;
        }
-       else{                // if j==k pop stack -> ans and reset j=0  
-         while(st.size()>0) ans[p++] = st.pop();
-         j = 0;
-       }
+       // updating pointers
+       w1 = w2+1;
+       w2 = w2+k;
+       l  = w1;
+       r  = w2;
      }
      
-     while(st.size()>0) ans[p++] = st.pop(); // left over
      
-     StringBuilder sb = new StringBuilder();
-     for(i=0; i<n; i++) sb.append(ans[i]+" ");
-     System.out.println(sb);
+     // left overs
+     if(w1 < n && w2 > n){
+       l = w1;
+       r = n-1;
+       while(l < r){
+         swap(arr, l, r);
+         l++; r--;
+       }
+     }
+   
+     // printing array
+     for(r=0; r<n; r++) System.out.print(arr[r]+" ");
    }
+   
    
    public static void main(String args[])
    { 
     int[] arr = {1,2,3,4,5};
     int k = 3;
-    reverse(arr, k);
+    reverse(arr, arr.length, k);
    }
  }
-
 
 
 // Ans = 3 2 1 5 4
