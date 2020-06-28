1eee5e62bc930c69901ebe9f911c9311ab9feea6
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-06-28 21:11:12 +0530

    Create 8 Remove Duplicates.java

---
+++ 2 Strings/8 Remove Duplicates.java
@@ -0,0 +1,51 @@
+/**
+
+Given a string, the task is to remove duplicates from it. Expected time complexity O(n) where n is length of input string and extra space O(1) under the assumption that there are total 256 possible characters in a string.
+
+Note: that original order of characters must be kept same. 
+
+Input:
+First line of the input is the number of test cases T. And first line of test case contains a string.
+
+Output: 
+Modified string without duplicates and same order of characters.
+
+Constraints: 
+1 <= T <= 15
+1 <= |string|<= 1000
+
+Example:
+Input:
+2
+geeksforgeeks
+geeks for geeks
+
+Output:
+geksfor
+geks for
+
+**/
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+ {
+    // USING C O N T A I N S METHOD OF STRING
+     
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while(t-->0){
+		    String s = br.readLine().trim();
+		    String ans = "";
+		    for(int i=0; i<s.length(); i++){
+		        if(!ans.contains(String.valueOf(s.charAt(i)))){
+		            ans += s.charAt(i);
+		        }
+		    }
+		    System.out.println(ans);
+		}
+	}
+}
