76f8074c34221d5b66a1e1d2d0217d3c46547287
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-06-28 11:21:57 +0530

    Create 1 Reverse Words in a given String.java

---
+++ 2 Strings/1 Reverse Words in a given String.java
@@ -0,0 +1,47 @@
+/**
+
+Given a String of length S, reverse the whole string without reversing the individual words in it. Words are separated by dots.
+
+Input:
+The first line contains T denoting the number of testcases. T testcases follow. Each case contains a string S containing characters.
+
+Output:
+For each test case, in a new line, output a single line containing the reversed String.
+
+Constraints:
+1 <= T <= 100
+1 <= |S| <= 2000
+
+Example:
+Input:
+2
+i.like.this.program.very.much
+pqr.mno
+
+Output:
+much.very.program.this.like.i
+mno.pqr
+
+
+**/
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+ {
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while(t-->0){
+		    String str[] = br.readLine().trim().split("\\.");
+		    StringBuilder sb = new StringBuilder();
+		    for(int i=str.length-1; i>=0; i--) sb.append(str[i] + ".");
+		    String ans = new String(sb.toString());
+		    int len = ans.length();
+		    System.out.println(ans.substring(0, len-1));
+		}
+	}
+}
