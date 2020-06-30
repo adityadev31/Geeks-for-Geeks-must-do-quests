60f18795b4b4b42e4298dbd1deae738b7ad7a1c8
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-06-30 11:38:23 +0530

    Create 2 Reverse a linked list.java

---
+++ 3 Linked List/2 Reverse a linked list.java
@@ -0,0 +1,97 @@
+/**
+
+Given a linked list of N nodes. The task is to reverse this list.
+
+Input:
+The first line of input contains the number of test cases T. For each test case, the first line contains the length of the linked list and the next line contains the elements of the linked list.
+
+Output:
+For each test case, print the reversed linked list in a new line.
+
+User Task:
+The task is to complete the function reverseList() with head reference as the only argument and should return new head after reversing the list.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= T <= 100
+1 <= N <= 104
+
+Example:
+Input:
+2
+6
+1 2 3 4 5 6
+5
+2 7 8 9 10
+Output:
+6 5 4 3 2 1
+10 9 8 7 2
+
+Explanation:
+Testcase 1: After reversing the list, elements are 6->5->4->3->2->1.
+Testcase 2: After reversing the list, elements are 10->9->8->7->2
+
+**/
+
+
+
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+    Node(int x){
+        this.data = x;
+        this.next = null;
+    }
+}
+
+public class Main {
+    
+    // print list
+    
+    public static void printList(Node head){
+        while(head!=null){
+            System.out.print(head.data+" ");
+            head = head.next;
+        }
+    }
+    
+    // reverse list function (recursive)
+    
+    public static Node reverse(Node head){
+        if(head.next == null || head == null) return head;
+        Node newHead = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        Node node7 = new Node(7);
+        Node node8 = new Node(8);
+        
+        Node head = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        node7.next = node8;
+        
+        head = reverse(head);
+        
+        printList(head);
+    }
+}
+
+// ans = 8 7 6 5 4 3 2 1
