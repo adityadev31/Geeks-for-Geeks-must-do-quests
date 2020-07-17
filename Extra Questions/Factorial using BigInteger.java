8ac95a7bfbdf15b3c441be6179ca9ce45695848a
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-07-17 12:04:24 +0530

    Create Factorial using BigInteger.java

---
+++ Extra Questions/Factorial using BigInteger.java
@@ -0,0 +1,37 @@
+/**
+
+BigInteger is a class in java reallllly helpful for solving extreeeemly large number operations.....
+
+BigInteger b = new BigInteger("1244634536");  // or any no. u want   NOTE BigInteger are immutables so we have to restore them again we make a change (same as string)
+
+b = b.multiply(new BigInteger("131232331"));
+
+similarly we can do all mathematical operations like add, div, mul, sub ;
+
+**/
+
+
+import java.util.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.math.BigInteger;
+
+
+public class Main {
+    
+    public static BigInteger fact(int n){
+        BigInteger result = BigInteger.ONE;
+        for(int i=2; i<=n; i++) result = result.multiply(BigInteger.valueOf(i));
+        return result;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt((br.readLine()).trim());
+        while(t-- > 0){
+            int n = Integer.parseInt((br.readLine()).trim());
+            System.out.println(fact(n));
+        }
+    }
+}
