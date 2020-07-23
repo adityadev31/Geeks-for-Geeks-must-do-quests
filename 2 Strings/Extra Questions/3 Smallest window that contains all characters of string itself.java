2772f89d0abb7bec26d783fccb64a151710bfb2b
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-07-23 14:15:18 +0530

    Create 3 Smallest window that contains all characters of string itself.java

---
+++ 2 Strings/Extra Questions/3 Smallest window that contains all characters of string itself.java
@@ -0,0 +1,10 @@
+// url - https://www.geeksforgeeks.org/smallest-window-contains-characters-string/
+
+/**
+  Approach - 
+
+  1) add chars in a set
+  2) make a window of set size and let set size = ST and set.clear()
+  3) iterate over the string with that window(size = ST) and if chars in window are distinct then print the window
+
+**/
