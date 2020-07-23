0bd4b3602c3d1f1fb319d54723063af2c3b87079
AdiTya-Dev31 <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-07-23 14:19:00 +0530

    Update 3 Smallest window that contains all characters of string itself.java

--- 2 Strings/Extra Questions/3 Smallest window that contains all characters of string itself.java
+++ 2 Strings/Extra Questions/3 Smallest window that contains all characters of string itself.java
@@ -7,4 +7,5 @@
   2) make a window of set size and let set size = ST and set.clear()
   3) iterate over the string with that window(size = ST) and if chars in window are distinct then print the window
 
+  T = O(n)
 **/
