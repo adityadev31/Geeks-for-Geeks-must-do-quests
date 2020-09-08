2e40aef16e1146f9ee662c2135dfe76aedc954d9
AdiTya Dev <52311859+AdiTya-Dev31@users.noreply.github.com>
2020-09-08 12:29:42 +0530

    Create Binary to integer.java

---
+++ 14 Bit Magic/Binary to integer.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String num = "1001";
+        System.out.println(Integer.parseInt(num, 2));
+    }
+}
+
+
+// ans  = 9
